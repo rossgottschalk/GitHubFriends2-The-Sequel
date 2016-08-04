8a43379ed29c724582da5092c499f49016fc5ed1
Ross Gottschalk <rossgottschalk@yahoo.com>
2016-08-03 20:41:13 -0400

    Initial Commit

---
+++ GitHubFriends2:The Sequel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubFriends2:The Sequel
+//
+//  Created by Ross Gottschalk on 8/3/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
